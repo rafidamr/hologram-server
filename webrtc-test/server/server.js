9bf451971d4baebcc042bdae39329ed68c5f5db2
Rei <18213027@std.stei.itb.ac.id>
2019-04-27 11:36:48 +0700

    yeeeeeee

---
+++ webrtc-test/server/server.js
@@ -0,0 +1,71 @@
+const HTTPS_PORT = 8443;
+
+const fs = require('fs');
+const https = require('https');
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+
+// Yes, TLS is required
+const serverConfig = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+};
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for the client html page
+const handleRequest = function(request, response) {
+  // Render the single client html file for any request the HTTP server receives
+  var p = request.url;
+  var r = [
+    new RegExp('/[.]{2}','g'),
+    new RegExp('/{2,}','g'),
+  ];
+  r.forEach(function(rr){
+    while(rr.test(p))
+      p.replace(rr,'/');
+  });
+  console.log('request received: ' + p);
+  if(p === '/') {
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(fs.readFileSync('client/index.html'));
+  } else if(p.length > 1 && p[0] == '/' && fs.existsSync(`client${p}`)) {
+    response.writeHead(200, {'Content-Type': 'application/javascript'});
+    response.end(fs.readFileSync(`client${p}`));
+  }
+};
+
+const httpsServer = https.createServer(serverConfig, handleRequest);
+httpsServer.listen(HTTPS_PORT, '0.0.0.0');
+
+// ----------------------------------------------------------------------------------------
+
+// Create a server for handling websocket calls
+const wss = new WebSocketServer({server: httpsServer});
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
+    // Broadcast any received message to all clients
+    console.log('received: %s', message);
+    wss.broadcast(message);
+  });
+  ws.on('open', function(e) {
+  });
+  ws.on('close', function(e) {
+  });
+});
+
+wss.broadcast = function(data) {
+  this.clients.forEach(function(client) {
+    if(client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+console.log('Server running. Visit https://localhost:' + HTTPS_PORT + ' in Firefox/Chrome.\n\n\
+Some important notes:\n\
+  * Note the HTTPS; there is no HTTP -> HTTPS redirect.\n\
+  * You\'ll also need to accept the invalid TLS certificate.\n\
+  * Some browsers or OSs may not allow the webcam to be used by multiple pages at once. You may need to use two different browsers or machines.\n'
+);
